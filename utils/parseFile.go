ab239db80fcd40e72b644201afbb26745d744f6b
moonyango <mosesadrian825@gmail.com>
2024-10-17 18:38:48 +0300

    feat: add ant colony struct, do a proper error handling, and change reading of file to line by line for more efficiency

--- utils/parseFile.go
+++ utils/parseFile.go
@@ -1,55 +1,98 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
+
+	"lem-in/models"
 )
 
-func ParseFile(filename string) {
-	filebytes, err := os.ReadFile(filename)
+func ParseFile(filename string) (*models.AntColony, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("err reading file:", err)
+		return nil, fmt.Errorf("error opening file: %v", err)
 	}
-	filestring := string(filebytes)
-	fileslice := strings.Split(filestring, "\n")
+	defer file.Close()
 
-	numberOfAnts := 0
-	roomMap := make(map[string][2]int)
-	linkMap := make(map[string]string)
+	scanner := bufio.NewScanner(file)
+	colony := &models.AntColony{
+		Rooms: make(map[string][2]int),
+		Links: make(map[string][]string),
+	}
 
-	for i, v := range fileslice {
-		if i == 0 {
-			n, err := strconv.Atoi(v)
+	lineNum := 0
+	var currentCommand string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineNum++
+
+		if lineNum == 1 {
+			colony.NumberOfAnts, err = strconv.Atoi(line)
 			if err != nil {
-				fmt.Println(err)
-				return
+				return nil, fmt.Errorf("invalid number of ants on line %d: %v", lineNum, err)
 			}
-			numberOfAnts = n
+			continue
+		}
+
+		if strings.HasPrefix(line, "#") {
+			if line == "##start" || line == "##end" {
+				currentCommand = line
+			}
+			continue
+		}
+
+		if strings.Contains(line, " ") {
+			// Room definition
+			parts := strings.Fields(line)
+			if len(parts) != 3 {
+				return nil, fmt.Errorf("invalid room definition on line %d", lineNum)
+			}
+
+			x, err := strconv.Atoi(parts[1])
+			if err != nil {
+				return nil, fmt.Errorf("invalid x-coordinate on line %d: %v", lineNum, err)
+			}
+
+			y, err := strconv.Atoi(parts[2])
+			if err != nil {
+				return nil, fmt.Errorf("invalid y-coordinate on line %d: %v", lineNum, err)
+			}
+
+			colony.Rooms[parts[0]] = [2]int{x, y}
+
+			if currentCommand == "##start" {
+				colony.Start = parts[0]
+				currentCommand = ""
+			} else if currentCommand == "##end" {
+				colony.End = parts[0]
+				currentCommand = ""
+			}
+		} else if strings.Contains(line, "-") {
+			// Link definition
+			parts := strings.Split(line, "-")
+			if len(parts) != 2 {
+				return nil, fmt.Errorf("invalid link definition on line %d", lineNum)
+			}
+
+			room1, room2 := parts[0], parts[1]
+			colony.Links[room1] = append(colony.Links[room1], room2)
+			colony.Links[room2] = append(colony.Links[room2], room1)
 		} else {
-			if strings.HasPrefix(v, "#") {
-				continue
-			} else {
-				if strings.Contains(v, " ") {
-					x_coord, err := strconv.Atoi(strings.Fields(v)[1])
-					if err != nil {
-						fmt.Println(err)
-					}
-					y_coord, err := strconv.Atoi(strings.Fields(v)[2])
-					if err != nil {
-						fmt.Println(err)
-					}
-					arr := [2]int{x_coord, y_coord}
-					roomMap[strings.Fields(v)[0]] = arr
-				}
-				if strings.Contains(v, "-") {
-					linkMap[strings.Split(v, "-")[0]] = strings.Split(v, "-")[1]
-				}
-			}
+			return nil, fmt.Errorf("invalid line format on line %d", lineNum)
 		}
 	}
-	fmt.Println(numberOfAnts)
-	fmt.Println(roomMap)
-	fmt.Println(linkMap)
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	if colony.Start == "" || colony.End == "" {
+		return nil, fmt.Errorf("start or end room not defined")
+	}
+
+	return colony, nil
 }
