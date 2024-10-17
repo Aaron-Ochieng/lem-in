6faf56c5b4583f2c184816cc1aa63e13f92fa004
moonyango <mosesadrian825@gmail.com>
2024-10-17 15:31:56 +0300

    fix: incorporate new function to the main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"lem-in/utils"
+)
+
 func main() {
+	filename, errmsg := utils.ParseArgs()
+	if errmsg != "" {
+		fmt.Println(errmsg)
+		return
+	}
+	fmt.Println(filename)
 }
