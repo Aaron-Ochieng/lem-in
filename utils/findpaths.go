e8cd627227637b229efb7beb83211a11295885b2
aosindo <andyovvo8@gmail.com>
2024-11-04 18:12:35 +0300

    chore: added chooseoptimum paths

--- utils/findpaths.go
+++ utils/findpaths.go
@@ -5,7 +5,7 @@ import (
 )
 
 // FindPaths finds all possible paths from start to end using BFS
-func FindPaths(colony *models.AntColony) []models.Path {
+func FindPaths(colony *models.AntColony) ([]models.Path,map[int][]int, int) {
 	var allPaths []models.Path
 	var queue [][]string
 	queue = append(queue, []string{colony.Start})
@@ -30,7 +30,7 @@ func FindPaths(colony *models.AntColony) []models.Path {
 			}
 		}
 	}
-	return allPaths
+	return ChooseOptimumPath(allPaths, colony)
 }
 
 // Helper function to check if a room is in a path
@@ -41,4 +41,46 @@ func containsRoom(path []string, room string) bool {
 		}
 	}
 	return false
+}
+
+func ChooseOptimumPath(paths []models.Path, Antcolony *models.AntColony) ([]models.Path, map[int][]int,int) {
+	shortest := OptimizePaths(paths)
+	shortopt := Optimize2(paths, Antcolony)
+	firstop := PlaceAnts(Antcolony, shortest)
+	secondop := PlaceAnts(Antcolony, shortopt)
+	turns1 := GenerateTurns(firstop,shortest)
+	turns2 := GenerateTurns(secondop, shortopt)
+	finalpath := shortest
+	finalAntspalced := firstop
+	turns := turns1
+	if turns1 > turns2 {
+		finalpath = shortopt
+		finalAntspalced = secondop
+		turns = turns2
+	}
+	return finalpath, finalAntspalced,turns
+}
+
+func OptimizePaths(paths []models.Path) []models.Path {
+	optimized := make([]models.Path, 0)
+	optimized = append(optimized, paths[0])
+	for i := 1; i < len(paths); i++ {
+		if Check(paths[i].Rooms, optimized) {
+			optimized = append(optimized, paths[i])
+		}
+	}
+	return optimized
+}
+
+func Check(path []string, optimized []models.Path) bool {
+	for _, optpath := range optimized {
+		for k := 1; k < len(path)-1; k++ {
+			for j :=1; j < len(optpath.Rooms)-1; j++{
+				if path[k] == optpath.Rooms[j] {
+					return false
+				}
+			}
+		}
+	}
+	return true
 }
