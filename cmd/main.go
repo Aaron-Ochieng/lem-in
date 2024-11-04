745f509df846df93f6d280c8bc84529ba2fd0b32
aosindo <andyovvo8@gmail.com>
2024-11-04 17:53:34 +0300

    chore: printed moves

--- cmd/main.go
+++ cmd/main.go
@@ -19,18 +19,11 @@ func main() {
 	}
 	paths := utils.FindPaths(Antcolony)
 	shortest := utils.OptimizePaths(paths)
-	fmt.Println(shortest)
 	shortopt := utils.Optimize2(paths, Antcolony)
-	fmt.Println(shortopt)
 	firstop := utils.PlaceAnts(Antcolony, shortest)
 	secondop := utils.PlaceAnts(Antcolony, shortopt)
-	fmt.Println(firstop)
-	fmt.Println(secondop)
-
 	turns1 := utils.GenerateTurns(firstop,shortest)
 	turns2 := utils.GenerateTurns(secondop, shortopt)
-    fmt.Println(turns1)
-	fmt.Println(turns2)
 	finalpath := shortest
 	finalAntspalced := firstop
 	turns := turns1
@@ -39,8 +32,8 @@ func main() {
 		finalAntspalced = secondop
 		turns = turns2
 	}
-	fmt.Println(finalpath)
-
 	moves := utils.MoveAnts(finalpath, finalAntspalced,turns)
-	fmt.Println(moves)
+	for _, move := range moves {
+		fmt.Println(move)
+	}
 }
--- utils/moveants.go
+++ utils/moveants.go
@@ -1,18 +1,18 @@
 package utils
 
-import "lem-in/models"
+import (
+	"fmt"
+	"lem-in/models"
+)
 
 
 func MoveAnts(paths []models.Path, antsperroom map[int][]int, turns int) []string {
 	moves := make([]string, turns)
-	count := 0
 	for i, p := range antsperroom {
 		path := paths[i].Rooms
 		for j , ant := range p {
-			count++
-			for k, room := range path {
-				count-1
-				moves[co]
+			for k, room := range path[1:] {
+				moves[j+k] += fmt.Sprintf("L%v-%v ",ant,room)
 			}
 		} 
 	}
