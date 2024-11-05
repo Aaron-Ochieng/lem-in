f49f6a2e25ccb5c473a2ce7e493b557fa6fc7f1e
aaochieng <ochiengaoron@outlook.com>
2024-11-05 11:33:52 +0300

    test: parsed values - Test if the temporary file is created sucessfully - Test number of ants parsed from the file - Test starting point of the ants - Test endpoint(room) for the ants - Test expected number of rooms - Test colony links

--- utils/parseFile_test.go
+++ utils/parseFile_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"testing"
 )
 
 // Mock data for tests
@@ -31,4 +32,35 @@ func createTempFile(data string) (*os.File, error) {
 		return nil, err
 	}
 	return tmpfile, nil
+}
+
+// Test for ParseFile function
+func TestParseFile(t *testing.T) {
+	tmpfile, err := createTempFile(testFileData)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	colony, err := ParseFile(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("ParseFile failed: %v", err)
+	}
+
+	// Assertions
+	if colony.NumberOfAnts != 5 {
+		t.Errorf("Expected 5 ants, got %d", colony.NumberOfAnts)
+	}
+	if colony.Start != "A" {
+		t.Errorf("Expected start room 'A', got '%s'", colony.Start)
+	}
+	if colony.End != "B" {
+		t.Errorf("Expected end room 'B', got '%s'", colony.End)
+	}
+	if len(colony.Rooms) != 3 {
+		t.Errorf("Expected 3 rooms, got %d", len(colony.Rooms))
+	}
+	if len(colony.Links["A"]) != 2 {
+		t.Errorf("Expected room 'A' to have 2 links, got %d", len(colony.Links["A"]))
+	}
 }
