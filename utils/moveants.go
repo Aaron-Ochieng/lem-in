a83a899b24f55aa35f7e1a294bfe4cff96cc5586
aosindo <andyovvo8@gmail.com>
2024-11-05 10:18:08 +0300

    chore: made sure proper arrangement of ants movements

--- utils/moveants.go
+++ utils/moveants.go
@@ -8,10 +8,10 @@ import (
 
 func MoveAnts(paths []models.Path, antsperroom map[int][]int, turns int) []string {
 	moves := make([]string, turns)
-	for i, p := range antsperroom {
-		path := paths[i].Rooms
-		for j , ant := range p {
-			for k, room := range path[1:] {
+	for i, path := range paths {
+		ants := antsperroom[i]
+		for j , ant := range ants {
+			for k, room := range path.Rooms[1:] {
 				moves[j+k] += fmt.Sprintf("L%v-%v ",ant,room)
 			}
 		} 
